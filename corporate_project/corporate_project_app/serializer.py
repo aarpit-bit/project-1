93e8d323d68fbea76eef954de6bb307aeb079224
Arpit Sharma <hello.arpit.s@gmail.com>
2024-10-28 14:32:30 +0530

    Update serializer.py

---
+++ corporate_project/corporate_project_app/serializer.py
@@ -0,0 +1,22 @@
+#importing necessary modules
+from rest_framework import serializers
+from .models import Company,Employee,Project
+
+#making a serializer for the company model for all fields
+class ComapnySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Company
+        fields='_all_'
+
+#making a serializer for the employee model for all fields
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Company
+        fields='_all_'
+
+
+#making a serializer for the project model for all fields
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Company
+        fields='_all_'
