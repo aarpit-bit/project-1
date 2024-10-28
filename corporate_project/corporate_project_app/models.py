d1ebf780104aa0c9750200db7c8fa771e035c160
aarpit-bit <hello.arpit.s@gmail.com>
2024-10-28 14:33:59 +0530

    adding comments

--- corporate_project/corporate_project_app/models.py
+++ corporate_project/corporate_project_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+
+### MADE COMPANY MODEL WITH ALL THE REQUIRED FIELDS SUCH AS COMPANY ID,NAME,DESCRIPTION,LOCATION,INDUSTRY,WEBSITE,COMPANY AGE,COMPANY NET WORTH,COMPANY FOUNDER,COMPANY HEADQUARTERS,COMPANY CEO,COMPANY EMPLOYEES,COMPANY PRODUCTS,COMPANY SERVICES,COMPANY MARKET CAP,COMPANY REVENUE,COMPANY PROFIT
 class Company(models.Model):
+    company_id=models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     location = models.CharField(max_length=255)
@@ -20,7 +23,10 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
+### MADE EMPLOYEE MODEL WITH ALL THE REQUIRED FIELDS SUCH AS EMPLOYEE ID,NAME,AGE,GENDER,SALARY,COMPANY,DEPARTMENT,DATE OF BIRTH,JOINING DATE,PHONE NUMBER,EMAIL,ADDRESS
 class Employee(models.Model):
+    company_id=models.ForeignKey(Company,on_delete=models.CASCADE)
+    employee_id=models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     age = models.IntegerField()
     gender = models.CharField(max_length=255)
@@ -36,10 +42,13 @@ class Employee(models.Model):
     def __str__(self):
         return self.name
     
+### MADE PROJECT MODEL WITH ALL THE REQUIRED FIELDS SUCH AS PROJECT ID,NAME,DESCRIPTION,START DATE,END DATE,COMPANY,BUDGET,STATUS,DURATION
 
-    
 class Project(models.Model):
     name=models.CharField(max_length=255)
+    company_id=models.ForeignKey(Company,on_delete=models.CASCADE)
+    project_id=models.AutoField(primary_key=True)
+    employee_id=models.ForeignKey(Employee,on_delete=models.CASCADE)
     description=models.TextField()
     start_date=models.DateField()
     end_date=models.DateField()
